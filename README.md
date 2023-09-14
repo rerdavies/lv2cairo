03495188f40fcd5ecae1f47d718998c934440f33
Robin Davies <rerdavies@gmail.com>
2023-09-14 15:29:11 -0400

    Setup documentation build.

--- README.md
+++ README.md
@@ -9,20 +9,8 @@ LVTK is a C++ class library for building LV2 plugins with custom user interfaces
 ![](website/public/img/SamplePlugin.png)
 
 
-[Web Page](https://rerdavies.github.io/lvtk)
+Go to the [Web Page](https://rerdavies.github.io/lvtk)
 
-[Documentation](https://rerdavies.github.io/lvtk#/documentation)
+Go to the [Documentation](https://rerdavies.github.io/lvtk#/documentation/index)
 
 
-Dependencies:
-
-```
-sudo apt install libcairo2-dev
-sudo apt install libpangocairo-1.0-0 (LGPL)
-sudo apt install cmake
-sudo apt install ninja-build (optional)
-sudo apt install catch2
-sudo apt install librsvg2-dev
-sudo apt install lv2-dev
-sudo apt install liblilv-dev
-```
--- website/src/VersionInfo.tsx
+++ website/src/VersionInfo.tsx
@@ -3,13 +3,12 @@
 const lv2cVersion_ : string = "0.0.0-prerelease";
 export function Lv2cVersion(): string { return lv2cVersion_; };
 
-export function Lv2cVersionNotice() : string { return `Please be advised that LVTK
-APIs are undergoing heavy revision as LVTK is being used to implement native 
-user interface for the ToobAmp project. 
+export function Lv2cVersionNotice() : string { return `
+In this pre-release version, LVTK APIs are still undergoing heavy revision while
+LVTK is being integrated into the Toob Amp project.
 
-If LVTK is something you are interested in, it would be wise to wait for a week or two
-before starting to incorporate it into any of your projects.
-
+Feel free to look araound; but it would be wise to wait a week or so for things to 
+settle down before using this project.
 `;
 }
 
