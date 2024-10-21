6eb84be1888dc3b6f32d373896442a022e5dca4f
Robin Davies <rerdavies@gmail.com>
2024-10-21 01:34:35 -0400

    Remove unstable api warning.

--- website/src/VersionInfo.tsx
+++ website/src/VersionInfo.tsx
@@ -3,13 +3,7 @@
 const lv2cVersion_ : string = "0.0.0-prerelease";
 export function Lv2cVersion(): string { return lv2cVersion_; };
 
-export function Lv2cVersionNotice() : string { return `
-In this pre-release version, Lv2Cairo APIs are still undergoing heavy revision while
-Lv2Cairo is being integrated into the Toob Amp project.
-
-Feel free to look araound; but it would be wise to wait a week or so for things to 
-settle down before using this project.
-`;
+export function Lv2cVersionNotice() : string { return ``;
 }
 
 const copyrightText: string = "Copyright © 2023 Robin Davies.";
